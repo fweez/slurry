45864b5018aaf19b409304980ce5a2e3ebfc3664
Ryan Forsythe <ryan@pdxcb.net>
2015-10-16 16:32:15 -0700

    HEY SLURRY STOP RESPONDING TO @ MESSAGES

--- slurry.py
+++ slurry.py
@@ -4,13 +4,22 @@ from slackbot.bot import Bot
 from slackbot.bot import respond_to, listen_to
 import re
 
-@listen_to('HI SLURRY')
+
+@listen_to('HI SLURRY', re.IGNORECASE)
 def hi(message):
     message.reply("Hello. I've hidden your keys.")
 
-@respond_to('HI SLURRY')
-def hir(message):
-    message.reply("You need to stop yelling, it's late and our neighbors need to be up for work tomorrow morning.")
+@listen_to('HEY SLURRY(.*)', re.IGNORECASE)
+def hey(message, contents=None):
+    if contents:
+        message.reply("I'm not sure" + contents + " is a good idea. Maybe you should have a glass of water?")
+    else:
+        message.reply("Again, I see you there. You don't need to keep yelling at me.")
+
+@respond_to('(.*)')
+def splat(message, contents=None):
+    # I don't want any @slurry responses, so:
+    message.reply("Hush now. Let's just lie down on the couch for a bit and chill.")
 
 def main():
     bot = Bot()
